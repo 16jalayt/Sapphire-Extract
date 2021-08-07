98ef0d89063fae7032deefad56b3f917f5d6cad0
Jason Layton <16jalayt@gmail.com>
2021-08-07 14:14:21 -0500

    Add project files.

---
+++ Sapphire Extract Common/Core.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Sapphire_Extract_Common
+{
+/// <summary>
+/// Common functions between the CLI and GUI
+/// </summary>
+    public class Core
+    {
+        //parse cmd line. go strait to extraction on gui? or set overwrite/etc?
+        //make cli class called Opts ie Opts.overwrite
+
+        //have variable input files. core should take in list or single file name and marshal 
+        //  the (later) threads and trigger the extraction events (also in core)
+
+
+
+
+
+        /// <summary>
+        /// Parse CLI arguments and init plugins.
+        /// </summary>
+        /// <param name="input">Temporary test param</param>
+        public static void init(string input)
+        {
+            Console.WriteLine(input);
+            string[] args = Environment.GetCommandLineArgs();
+            foreach(string arg in args)
+            {
+                Console.WriteLine(arg);
+            }
+            
+        }
+    }
+}
---
+++ Sapphire Extract/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Sapphire_Extract_Common;
+
+namespace Sapphire_Extract
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Core.init("Hello World! From init");
+        }
+    }
+}
